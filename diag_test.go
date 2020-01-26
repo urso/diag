450ded2463d424f52b87ac30e3e48fbb4895ef19
urso <steffen.siering@elastic.co>
2020-01-26 19:30:34 +0100

    Add support for context.Context

--- diag_test.go
+++ diag_test.go
@@ -1,19 +1,10 @@
 package diag_test
 
 import (
-	"strings"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
 
 	"github.com/urso/diag"
 )
-
-type testVisitor struct {
-	M     map[string]interface{}
-	keys  []string
-	stack []map[string]interface{}
-}
 
 func TestCtxBuild(t *testing.T) {
 	t.Run("new empty context", func(t *testing.T) {
@@ -324,83 +315,4 @@ func TestCtxVisitStructured(t *testing.T) {
 			"d": 6,
 		},
 	}, v.Get())
-}
-
-func makeCtx(before, after *diag.Context, vs ...interface{}) *diag.Context {
-	ctx := diag.NewContext(before, after)
-	ctx.AddAll(vs...)
-	return ctx
-}
-
-func assertCtx(t *testing.T, want map[string]interface{}, ctx *diag.Context) {
-	t.Helper()
-
-	var v testVisitor
-	ctx.VisitStructured(&v)
-	requireEqual(t, want, v.Get())
-}
-
-func assertFlatCtx(t *testing.T, want map[string]interface{}, ctx *diag.Context) {
-	t.Helper()
-
-	var v testVisitor
-	ctx.VisitKeyValues(&v)
-	requireEqual(t, want, v.Get())
-}
-
-func requireEqual(t *testing.T, want, has interface{}, msg ...string) {
-	t.Helper()
-	if diff := cmp.Diff(want, has); diff != "" {
-		if len(msg) > 0 {
-			t.Fatalf("%v -- missmatch (-want +got):\n%s", diff, strings.Join(msg, " "))
-		}
-		t.Fatalf("missmatch (-want +got):\n%s", diff)
-	}
-}
-
-func requireNoError(t *testing.T, err error) {
-	t.Helper()
-	if err != nil {
-		t.Fatalf("require no error fail with: %+v", err)
-	}
-}
-
-func (v *testVisitor) Get() map[string]interface{} {
-	return v.M
-}
-
-func (v *testVisitor) OnValue(key string, val diag.Value) error {
-	if v.M == nil {
-		v.M = map[string]interface{}{}
-	}
-
-	v.M[key] = val.Interface()
-	return nil
-}
-
-func (v *testVisitor) OnObjStart(key string) error {
-	v.keys = append(v.keys, key)
-	v.stack = append(v.stack, v.M)
-	v.M = nil
-	return nil
-}
-
-func (v *testVisitor) OnObjEnd() error {
-	keysEnd := len(v.keys) - 1
-	key := v.keys[keysEnd]
-	v.keys = v.keys[:keysEnd]
-
-	m := v.M
-	mapsEnd := len(v.stack) - 1
-	v.M = v.stack[mapsEnd]
-	v.stack = v.stack[:mapsEnd]
-
-	if m != nil {
-		if v.M == nil {
-			v.M = map[string]interface{}{}
-		}
-		v.M[key] = m
-	}
-
-	return nil
 }
---
+++ gocontext_test.go
@@ -0,0 +1,70 @@
+package diag_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/urso/diag"
+)
+
+func TestStdlibContext(t *testing.T) {
+	isContext := func(t *testing.T, ctx context.Context, dc *diag.Context) {
+		t.Helper()
+		has, ok := diag.DiagnosticsFrom(ctx)
+		if !ok {
+			t.Fatal("no diagnostic context found")
+		}
+		if dc != has {
+			t.Fatal("unexpected diagnostic context returned")
+		}
+	}
+
+	assertDiagnostics := func(t *testing.T, want map[string]interface{}, ctx context.Context) {
+		t.Helper()
+		dc, ok := diag.DiagnosticsFrom(ctx)
+		if !ok {
+			t.Fatal("no diagnostic context found")
+		}
+		assertCtx(t, want, dc)
+	}
+
+	t.Run("fail if no diagnostic context is stored", func(t *testing.T) {
+		_, ok := diag.DiagnosticsFrom(context.Background())
+		if ok {
+			t.Fatal("context indicator should be false")
+		}
+	})
+
+	t.Run("add diagnostics", func(t *testing.T) {
+		dc := makeCtx(nil, nil, "a", 1)
+		ctx := diag.NewDiagnostics(context.Background(), dc)
+		isContext(t, ctx, dc)
+	})
+
+	t.Run("new context overwrites old", func(t *testing.T) {
+		dc1 := makeCtx(nil, nil, "a", 1)
+		dc2 := makeCtx(nil, nil, "a", 2)
+		ctx := diag.NewDiagnostics(context.Background(), dc1)
+		ctx = diag.NewDiagnostics(ctx, dc2)
+		isContext(t, ctx, dc2)
+	})
+
+	t.Run("push creates a new context", func(t *testing.T) {
+		ctx := diag.PushDiagnostics(context.Background(), "a", 1)
+		assertDiagnostics(t, map[string]interface{}{"a": 1}, ctx)
+	})
+
+	t.Run("push references existing context", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = diag.PushDiagnostics(ctx, "a", 1)
+		ctx = diag.PushDiagnostics(ctx, "b", 2)
+		assertDiagnostics(t, map[string]interface{}{"a": 1, "b": 2}, ctx)
+	})
+
+	t.Run("push shadows existing fields", func(t *testing.T) {
+		ctx := context.Background()
+		ctx = diag.PushDiagnostics(ctx, "a", 1)
+		ctx = diag.PushDiagnostics(ctx, "a", 2)
+		assertDiagnostics(t, map[string]interface{}{"a": 2}, ctx)
+	})
+}
